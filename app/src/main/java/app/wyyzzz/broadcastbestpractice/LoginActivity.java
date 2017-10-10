5db38cfd923e9a25cf1951cdd35f3f1e72c32ba9
吴远洋 <wuyuanyang@wuyuanyangdeMacBook-Pro.local>
2017-10-10 14:26:51 +0800

    remeber password by sharedpreference

--- app/src/main/java/app/wyyzzz/broadcastbestpractice/LoginActivity.java
+++ app/src/main/java/app/wyyzzz/broadcastbestpractice/LoginActivity.java
@@ -1,14 +1,21 @@
 package app.wyyzzz.broadcastbestpractice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends BaseActivity {
+
+    private SharedPreferences pref;
+
+    private SharedPreferences.Editor editor;
 
     private EditText accountEdit;
 
@@ -16,14 +23,28 @@ public class LoginActivity extends BaseActivity {
 
     private Button login;
 
+    private CheckBox rememberPass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
         accountEdit = (EditText)findViewById(R.id.account);
         passwordEdit = (EditText)findViewById(R.id.password);
+        rememberPass = (CheckBox)findViewById(R.id.remeber_pass);
         login = (Button)findViewById(R.id.login);
+        boolean isRemember = pref.getBoolean("remember_password",false);
+
+        if(isRemember){
+            //将账户和密码都设置到文本框中
+            String account = pref.getString("account","");
+            String password = pref.getString("password","");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
 
         login.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -33,11 +54,21 @@ public class LoginActivity extends BaseActivity {
 
                 //如果账号是admin，且密码是123456，就认为登陆成功
                 if(account.equals("admin")&&password.equals("123456")){
+                    editor = pref.edit();
+                    if(rememberPass.isChecked()){
+                        //检查复选框是否被选中
+                        editor.putBoolean("remember_password", true);
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+                    }else{
+                        editor.clear();
+                    }
+                    editor.apply();
                     Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(intent);
                     finish();
                 }else{
-                    Toast.makeText(LoginActivity.this,"account or password is invalid",Toast.LENGTH_SHORT);
+                    Toast.makeText(LoginActivity.this,"account or password is invalid",Toast.LENGTH_SHORT).show();
                 }
             }
         });
